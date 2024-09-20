5eb1d1e6cde15a331d3dcd12c110e1c4390608eb
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2024-09-20 07:48:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,19 @@
 variable "resource_group_name" {
   type    = string
-  default = "multi-sa-rg"
 }
 
 variable "location" {
   type    = string
-  default = "uksouth"
 }
 
 variable "storage_account_name" {
   type    = string
-  default = "Saanvikit2024"
 }
 
 variable "tags" {
   type = map(string)
-  default = {
-    project     = "saanvikit"
-    environment = "dev"
-  }
 }
 
 variable "count_value" {
   type    = number
-  default = 50
 }
