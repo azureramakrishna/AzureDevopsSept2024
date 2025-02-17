8100e3685bef1bbd825d69636e01af0b6bb4db36
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2025-02-17 22:04:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,20 @@
 variable "resource_group_name" {
   type    = string
+  default = "storage-rg-01"
 }
 
 variable "location" {
   type    = string
+  default = "eastus"
 }
 
 variable "storage_account_name" {
   type    = string
+  default = "saanvikit"
 }
 
 variable "tags" {
   type = map(string)
 }
 
-variable "count_value" {
-  type    = number
-}
+
