3bcbf89cb30591363dffe65e8e13bf3672b85bf0
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2025-02-17 22:03:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "saanvikit-group"
+    storage_account_name  = "saanvikit"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+    use_oidc              = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
+
+
 #############################################
 # create a resource group
 #############################################
